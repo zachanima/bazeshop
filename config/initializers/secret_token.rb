21b457d1ca76ad204bbd9d7b9bd8bb387a4154ff
Zach Ewert <zachanima@gmail.com>
2012-08-04 14:21:19 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bazeshop::Application.config.secret_token = '2a756a81598bb7bf441ea3f7a671f85a5556eb76d3f1765081ef96c67ab91528a5fd3267ac9e38143b22addba2ba76e3f9e31accf2274b8a7fa4393800ba7d8a'
