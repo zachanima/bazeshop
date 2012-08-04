86575de253c84544db2928386bf9debb7e11e5c4
Zach Ewert <zachanima@gmail.com>
2012-08-04 14:56:41 +0200

    Refactor shop tests

--- test/unit/shop_test.rb
+++ test/unit/shop_test.rb
@@ -1,32 +1,31 @@
 require 'test_helper'
 
 class ShopTest < ActiveSupport::TestCase
+  def setup
+    @shop = FactoryGirl.build(:shop)
+  end
+
   test 'should save shop on valid input' do
-    shop = FactoryGirl.build(:shop)
-    assert shop.save
+    assert @shop.save
   end
 
   test 'should not save shop without name' do
-    shop = FactoryGirl.build(:shop)
-    shop.name = nil
-    assert !shop.save
+    @shop.name = nil
+    assert !@shop.save
   end
 
   test 'should not save shop with empty name' do
-    shop = FactoryGirl.build(:shop)
-    shop.name = ''
-    assert !shop.save
+    @shop.name = ''
+    assert !@shop.save
   end
 
   test 'should not save shop without link' do
-    shop = FactoryGirl.build(:shop)
-    shop.link = nil
-    assert !shop.save
+    @shop.link = nil
+    assert !@shop.save
   end
 
   test 'should not save shop with empty link' do
-    shop = FactoryGirl.build(:shop)
-    shop.link = ''
-    assert !shop.save
+    @shop.link = ''
+    assert !@shop.save
   end
 end
