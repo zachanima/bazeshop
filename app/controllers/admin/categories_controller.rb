8f2cd70fb17bb140be2f062fc15219a2c3514370
Zach Ewert <zachanima@gmail.com>
2012-08-06 07:45:15 +0200

    Add categories new view, create action

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -2,9 +2,22 @@ class Admin::CategoriesController < Admin::ApplicationController
   before_filter :find_shop
 
   def index
-    @categories = Category.where parent_id: nil
+    @categories = @shop.categories.where parent_id: nil
   end
 
+  def new
+    @category = @shop.categories.build
+  end
+
+  def create
+    @category = @shop.categories.build params[:category]
+    if @category.save
+      redirect_to admin_shop_categories_path(@shop), notice: 'Created category.'
+    else
+      flash[:error] = 'Error while creating category.'
+      render :new
+    end
+  end
 private
   def find_shop
     @shop = Shop.find params[:shop_id]
--- test/integration/admin/categories_test.rb
+++ test/integration/admin/categories_test.rb
@@ -9,4 +9,15 @@ class Admin::CategoriesTest < ActionDispatch::IntegrationTest
       assert has_content? 'foo'
     end
   end
+
+  test 'creating with valid input' do
+    shop = FactoryGirl.create(:shop)
+    visit admin_root_path
+    click_link 'Categories'
+    click_link 'New category'
+    fill_in 'Name', with: 'foo'
+    select '', from: 'Parent'
+    click_button 'Create Category'
+    assert_equal admin_shop_categories_path(shop), current_path
+  end
 end
