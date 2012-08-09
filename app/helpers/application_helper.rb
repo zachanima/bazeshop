33b513ff6da83c9b74a3dad931f3b8c75d1cf88e
Zach Ewert <zachanima@gmail.com>
2012-08-09 04:20:47 +0200

    Add number to smart currency method to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def current_shop? shop
     params[:shop_id].to_i == shop.id or (controller.controller_name == 'shops' and params[:id].to_i == shop.id)
   end
+
+  def number_to_smart_currency number, currency = 'kr.'
+    if number % 1 == 0
+      "#{number.to_i} #{currency}"
+    else
+      number_to_currency number, unit: currency, format: '%n %u'
+    end
+  end
 end
