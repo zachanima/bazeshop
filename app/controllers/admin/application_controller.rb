0eb8c59d1a4185242ec67cbdcb2b1f9a82c7a62e
Zach Ewert <zachanima@gmail.com>
2012-09-26 06:43:00 +0200

    Add administrator authentication

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,2 +1,9 @@
 class Admin::ApplicationController < ApplicationController
+  before_filter :authenticate!
+
+  def authenticate!
+    authenticate_or_request_with_http_basic do |login, password|
+      login == 'login' && password == 'password'
+    end
+  end
 end
