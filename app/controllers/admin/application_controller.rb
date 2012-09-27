bcb2fb32485108342a470d0729e7e6f060705c2b
Zach Ewert <zachanima@gmail.com>
2012-09-27 19:53:12 +0200

    Rename application layout to admin

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,5 +1,6 @@
 class Admin::ApplicationController < ApplicationController
   before_filter :authenticate!
+  layout 'admin'
 
   def authenticate!
     authenticate_or_request_with_http_basic do |login, password|
