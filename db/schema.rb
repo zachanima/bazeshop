c086390bb4efd8633c95431f4e01ebb0e96f5f3d
Zach Ewert <zachanima@gmail.com>
2012-09-28 20:27:39 +0200

    Add address to users resource

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120928182500) do
+ActiveRecord::Schema.define(:version => 20120928182653) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -118,6 +118,7 @@ ActiveRecord::Schema.define(:version => 20120928182500) do
     t.string   "department"
     t.string   "name"
     t.string   "phone"
+    t.text     "address"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
