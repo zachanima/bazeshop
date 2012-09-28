b6bb943f5c371ef09f122cf93d3b021403efa481
Zach Ewert <zachanima@gmail.com>
2012-09-28 20:16:29 +0200

    Remove comments from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,11 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, # :registerable, :recoverable,
-    :rememberable, :trackable, :validatable, authentication_keys: [:login, :shop_id]
+  devise :database_authenticatable, :rememberable, :trackable, :validatable,
+    authentication_keys: [:login, :shop_id]
 
   belongs_to :shop
 
-  # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me,
     :login, :plaintext_password, :department, :name
-  # attr_accessible :title, :body
 
   validates :login, presence: true
   validates :name, presence: true
