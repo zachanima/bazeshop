953f0e387b6084839e436fcbbe8c35dfe4c13b25
Zach Ewert <zachanima@gmail.com>
2012-09-28 20:14:51 +0200

    Add name to users resource

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,11 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me,
-    :login, :plaintext_password, :department
+    :login, :plaintext_password, :department, :name
   # attr_accessible :title, :body
 
   validates :login, presence: true
+  validates :name, presence: true
 
   def email_required?
     false
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120928181113) do
+ActiveRecord::Schema.define(:version => 20120928181302) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -116,6 +116,7 @@ ActiveRecord::Schema.define(:version => 20120928181113) do
     t.integer  "shop_id"
     t.string   "plaintext_password"
     t.string   "department"
+    t.string   "name"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
