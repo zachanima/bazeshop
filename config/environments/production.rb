2358d2d2b263d84bfd0c1a08f57a6914670a38b9
Zach Ewert <zachanima@gmail.com>
2012-09-28 09:06:40 +0200

    Add style.css to precompile assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,8 @@ Bazeshop::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  config.assets.precompile += ['style.css']
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
