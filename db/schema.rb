f6c7898ba48d3a24ed0dfa14d54d69ec7a03f0cb
Zach Ewert <zachanima@gmail.com>
2012-09-29 23:18:33 +0200

    Add comment to order

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120929202708) do
+ActiveRecord::Schema.define(:version => 20120929211332) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -84,6 +84,7 @@ ActiveRecord::Schema.define(:version => 20120929202708) do
     t.integer  "user_id"
     t.datetime "created_at",                                :null => false
     t.datetime "updated_at",                                :null => false
+    t.text     "comment"
   end
 
   create_table "products", :force => true do |t|
