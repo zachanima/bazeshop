b287a03993e6c788bb931eb0d08db1eaf9345ee4
Zach Ewert <zachanima@gmail.com>
2014-04-21 10:16:10 +0200

    Ensure all existing users are migrated as active

--- db/migrate/20140421074328_add_is_active_to_users.rb
+++ db/migrate/20140421074328_add_is_active_to_users.rb
@@ -1,5 +1,13 @@
 class AddIsActiveToUsers < ActiveRecord::Migration
-  def change
+  def up
     add_column :users, :is_active, :boolean
+    User.all.each do |user|
+      user.is_active = true
+      user.save
+    end
+  end
+
+  def down
+    remove_column :users, :is_active
   end
 end
