f59a686aa23ced366084dd22de58f31f9810c5c9
Emma Ewert <EmmaEwert@users.noreply.github.com>
2017-12-05 12:44:39 +0100

    Sign out users in wrong shops

--- app/controllers/user/sessions_controller.rb
+++ app/controllers/user/sessions_controller.rb
@@ -1,7 +1,14 @@
 class User::SessionsController < Devise::SessionsController
   before_filter :find_shop
+  before_filter :sign_out_on_wrong_shop
 
   def new
+  end
+  
+  def sign_out_on_wrong_shop
+    if current_user and current_user.shop != @shop
+      sign_out_and_redirect(current_user)
+    end
   end
 
 private
